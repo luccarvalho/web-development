6df53a89124aa892cb1f39feb8335e05e329f1af
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-11-10 12:55:33 -0300

    Testando operações assíncronas.

--- index.js
+++ index.js
@@ -6,15 +6,39 @@ function trataErro(erro) {
   throw new Error(chalk.red(erro.code, "Não há arquivo no diretório"));
 }
 
-function pegaArquivo(caminhoDoArquivo) {
-  const encoding = "utf-8";
-  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-    if (erro) {
-      trataErro(erro);
-    }
+// async/await
 
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = "utf-8";
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
     console.log(chalk.green(texto));
-  });
+  } catch (erro) {
+    trataErro(erro);
+  }
 }
+
+// Promises com .them()
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = "utf-8";
+//   fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(chalk.green(texto)))
+//     .catch(trataErro);
+// }
+
+// Código síncrono
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = "utf-8";
+//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//     if (erro) {
+//       trataErro(erro);
+//     }
+
+//     console.log(chalk.green(texto));
+//   });
+// }
 
 pegaArquivo("./arquivos/texto.md");
