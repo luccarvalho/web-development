665c38754ffb4187acfdc865051336ac02daaac7
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-11-11 13:04:40 -0300

    Iniciando com expressões regulares.

---
+++ regularExpression.js
@@ -0,0 +1,23 @@
+import fs from "fs";
+import chalk from "chalk";
+
+function trataErro(erro) {
+  console.log(erro);
+  throw new Error(chalk.red(erro.code, "Não há arquivo no diretório"));
+}
+
+// async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = "utf-8";
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    console.log(chalk.green(texto));
+  } catch (erro) {
+    trataErro(erro);
+  }
+}
+
+// Expressões regulares
+
+// \[[^[\]*?\]
