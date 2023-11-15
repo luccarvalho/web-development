e000c36c9125f9dd193f24db6337c9e3a94eec9d
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-11-15 12:11:10 -0300

    Organizando responsabilidades de funções.

--- src/regularExpression.js
+++ src/regularExpression.js
@@ -5,7 +5,9 @@ function extraiLinks(texto) {
   const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm; // Expressões regulares
   const capturas = [...texto.matchAll(regex)];
   const resultados = capturas.map((captura) => ({ [captura[1]]: captura[2] }));
-  return resultados;
+  return resultados.length !== 0
+    ? resultados
+    : chalk.red("Não há links no arquivo");
 }
 
 function trataErro(erro) {
@@ -19,7 +21,7 @@ async function pegaArquivo(caminhoDoArquivo) {
   try {
     const encoding = "utf-8";
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-    console.log(extraiLinks(texto));
+    return extraiLinks(texto);
   } catch (erro) {
     trataErro(erro);
   }
