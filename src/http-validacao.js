368293b9959b91782107be6020e3c2eb786c3dac
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-11-26 14:29:40 -0300

    Validando links.

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -2,6 +2,20 @@ function extraiLinks(arrayLinks) {
   return arrayLinks.map((objetoLink) => Object.values(objetoLink).join());
 }
 
+async function checaStatus(listaURLs) {
+  const arrayStatus = await Promise.all(
+    listaURLs.map(async (url) => {
+      try {
+        const response = await fetch(url);
+        return `${response.status} - ${response.statusText}`;
+      } catch (erro) {
+        return manejaErros(erro);
+      }
+    })
+  );
+  return arrayStatus;
+}
+
 export default async function listaValidada(listaDeLinks) {
   const links = extraiLinks(listaDeLinks);
   const status = await checaStatus(links);
