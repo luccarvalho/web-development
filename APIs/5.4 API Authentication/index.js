9d94d3b3caa3af5be108ea109a978dd34c455de1
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2024-04-08 13:49:05 -0300

    Adicionando novo módulo de estudo.

---
+++ APIs/5.4 API Authentication/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
+
+//TODO 1: Fill in your values for the 3 types of auth.
+const yourUsername = "chriscornell";
+const yourPassword = "rockforever123";
+const yourAPIKey = "5a47ea13-d903-4c60-b075-460a7241b5be";
+const yourBearerToken = "f2a9fbd7-048d-48c5-a842-5ff1adf2d634";
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "API Response." });
+});
+
+app.get("/noAuth", async (req, res) => {
+  //TODO 2: Use axios to hit up the /random endpoint
+  //The data you get back should be sent to the ejs file as "content"
+  //Hint: make sure you use JSON.stringify to turn the JS object from axios into a string.
+  try {
+    const result = await axios.get(API_URL + "/random");
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send("Error:", error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
