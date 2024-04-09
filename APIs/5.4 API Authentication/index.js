ef1358c27f50acaf75bb4c4c9bda88351ecf1acd
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2024-04-09 16:46:32 -0300

    Continuação do desenvolvimento do módulo atual de estudo.

--- APIs/5.4 API Authentication/index.js
+++ APIs/5.4 API Authentication/index.js
@@ -44,6 +44,24 @@ app.get("/basicAuth", async (req, res) => {
   }
 });
 
+app.get("/apiKey", async (req, res) => {
+  //TODO 4: Write your code here to hit up the /filter endpoint
+  //Filter for all secrets with an embarassment score of 5 or greater
+  //HINT: You need to provide a query parameter of apiKey in the request.
+  try {
+    const result = await axios.get(API_URL + "/filter", {
+      params: {
+        score: 5,
+        apiKey: yourAPIKey,
+      },
+    });
+
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send("Error:", error.message);
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
