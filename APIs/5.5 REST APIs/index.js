a44094ec69d20b43f3ca153d423804a152e4ed7b
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2024-04-15 12:04:01 -0300

    Continuação do desenvolvimento do módulo atual de estudo.

--- APIs/5.5 REST APIs/index.js
+++ APIs/5.5 REST APIs/index.js
@@ -34,6 +34,31 @@ app.post("/get-secret", async (req, res) => {
   }
 });
 
+app.post("/post-secret", async (req, res) => {
+  // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
+  try {
+    const result = await axios.post(API_URL + "/secrets" + req.body, config);
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
+});
+
+app.post("/put-secret", async (req, res) => {
+  // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
+  const searchId = req.body.id;
+  try {
+    const result = await axios.put(
+      API_URL + "/secrets/" + searchId,
+      req.body,
+      config
+    );
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
