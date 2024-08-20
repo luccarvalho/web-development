99ae1a38159b1606db0319dc02bd56c93da9be12
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2024-08-20 12:35:41 -0300

    Continuação do desenvolvimento do módulo atual de estudo.

--- JavaScript/Pig-Game/script.js
+++ JavaScript/Pig-Game/script.js
@@ -23,3 +23,36 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
+
+// Função para alternar entre os jogadores
+const switchPlayer = function () {
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// Função para rolar os dados
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // 1° Gerando um número aleatório entre 1 e 6
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+
+    // 2° Mostrando o número do dado
+    diceEl.classList.remove('hidden');
+    diceEl.src = `img/dice-${dice}.png`;
+
+    // 3° Conferindo se o número gerado é igual a 1
+    if (dice !== 1) {
+      // Soma esse número a pontuação atual
+      currentScore += dice;
+      document.querySelector(`#current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Alterna entre o primeiro e segundo jogador
+      switchPlayer();
+    }
+  }
+});
