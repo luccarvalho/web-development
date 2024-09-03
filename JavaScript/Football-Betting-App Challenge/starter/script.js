bbefa60a59b48c5e1f9006c517683c9b5be6e8f9
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2024-09-03 12:38:25 -0300

    Continuação do desenvolvimento do módulo atual de estudo.

--- JavaScript/Football-Betting-App Challenge/starter/script.js
+++ JavaScript/Football-Betting-App Challenge/starter/script.js
@@ -51,8 +51,7 @@ Your tasks:
 1. Loop over the game.scored array and print each player name to the console,
 along with the goal number (Example: "Goal 1: Lewandowski")
 
-2. Use a loop to calculate the average odd and log it to the console (We already
-studied how to calculate averages, you can go check if you don't remember)
+2. Use a loop to calculate the average odd and log it to the console
 
 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
 Odd of victory Bayern Munich: 1.33
